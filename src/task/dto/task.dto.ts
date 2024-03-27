91b12f98441e3fbf76b9c9be7071d7fcede42375
RanylFoumbi <ranylfoumbi@gmail.com>
2024-03-27 23:09:18 +0100

    updates

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,13 +1,7 @@
-import { IsNotEmpty, IsNumberString } from 'class-validator';
-
 export class CreateTaskDto {
-    @IsNotEmpty()
     name: string;
 
-    @IsNotEmpty()
     userId: string;
 
-    @IsNotEmpty()
-    @IsNumberString()
     priority: number;
 }
